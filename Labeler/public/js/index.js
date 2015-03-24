be5514ccd735144f2b85fd97f36bfaa835804499
dongchao <dongchao@xiaomi.com>
2015-03-24 16:46:31 +0900

    add email

--- Labeler/public/js/index.js
+++ Labeler/public/js/index.js
@@ -31,16 +31,16 @@ validateForm.ready = function () {
   ;
 
   $('.ui.form#login').form({
-        login_username: {
+      login_username: {
           identifier : 'login_username',
           rules: [
-            {
+              {
               type   : 'empty',
               prompt : 'Please enter a username'
             }
           ]
         },
-        login_password: {
+      login_password: {
           identifier : 'login_password',
           rules: [
             {
@@ -52,13 +52,26 @@ validateForm.ready = function () {
               prompt : 'Your password must be at least 3 characters'
             }
           ]
-        }
+      },
+      login_email : {
+          identifier: "login_email",
+          rules: [
+              {
+                  type: "email",
+                  prompt: "the input format must be suitable of email"
+              },
+              {
+                  type : "empty",
+                  prompt: "Please input an  email"
+              }
+          ]}
       },{
         onSuccess: function (event) {
-          var inputlist = $(event.target).find("input");
-          var username = inputlist[0].value;
-          var password = inputlist[1].value;
-          var form = $(event.target).attr("id");
+            var inputlist = $.find("#login input");
+            var username = inputlist[0].value;
+            var email = inputlist[1].value;
+            var password = inputlist[2].value;
+            var form = $(event.target).attr("id");
           if (form === "login") {
             $.ajax({
               type: 'POST',
@@ -83,7 +96,72 @@ validateForm.ready = function () {
               },
               dataType: 'json'
             });
-          } else if (form === "register") {
+          }
+        }
+      });
+
+
+    $(".ui.form#register").form({
+        register_username: {
+          identifier : 'register_username',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter a username'
+            }
+          ]
+        },
+        register_email : {
+            identifier: "login_email",
+            rules: [
+                {
+                    type: "email",
+                    prompt: "the input format must be suitable of email"
+                },
+                {
+                    type : "empty",
+                    prompt: "Please input an  email"
+                }
+            ]},
+        register_password: {
+          identifier : 'login_password',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter a password'
+            },
+            {
+              type   : 'length[3]',
+              prompt : 'Your password must be at least 3 characters'
+            }
+          ]
+        },
+        register_confirm_password: {
+            identifier: "register_confirm_password",
+            rules: [
+                {
+                    type : "empty",
+                    prompt: "Please enter a password"
+                },
+                {
+                    type: "length[6]",
+                    prompt: "Your password must be at least 6 characters"
+                },
+                {
+                    type: "match[register_password]",
+                    prompt: "The confirm password must be equal to the password origin"
+                }
+            ]
+        }
+
+      },{
+        onSuccess: function (event) {
+            var inputlist = $.find("#register input");
+            var username = inputlist[0].value;
+            var email = inputlist[1].value;
+            var password = inputlist[2].value;
+            var form = $(event.target).attr("id");
+          if (form === "register") {
             $.ajax({
               type: 'POST',
               url: '/register',
@@ -104,7 +182,7 @@ validateForm.ready = function () {
             });
           }
         }
-      });
+    });
 };
 
 
