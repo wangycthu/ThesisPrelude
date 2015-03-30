aaa9e1d3dcb3edc52ee73dee0059a4b319ac1716
dongchao <dongchao@xiaomi.com>
2015-03-30 14:45:30 +0900

    add logger

--- Labeler/routes/check.js
+++ Labeler/routes/check.js
@@ -7,14 +7,15 @@ var router = express.Router();
 var async = require("async");
 var samples = require("../models/samples");
 var config = require("../config");
+var logger = require("../models/logger");
 var Thread = require("./thread");
 router.get('/', function(req, res, next){
 
-    console.log("check");
+
     var token = req.session.user;
     if(token === undefined) {
 
-        console.log("not login");
+        logger.info("not login");
         res.redirect("/index");
     }
     var _username = req.session.username;
@@ -26,12 +27,13 @@ router.get('/', function(req, res, next){
     var keywords = config.keywordList;
     samples.getCountofIdsByConflict(_keyword, function(status, amount){
         samples.getSamplesByConflict(_keyword, function(status, rows){
-            console.log(rows);
+
+            logger.info(rows);
             // set cookies
             if(rows.length) {
 
                 req.session.threadID = rows[0]["id"];
-                console.log(rows[0]["id"]);
+                logger.info(rows[0]["id"]);
                 req.session.keyword = _keyword;
             }
             res.render("check",
@@ -44,48 +46,46 @@ router.get('/', function(req, res, next){
                        });
         });
     });
-    /**
-    samples.getSamplesByConflict(_keyword, function(status, rows){
-        console.log(rows);
-        res.render("check",
-                   {
-                       title: config.title,
-                       keyword: _keyword,
-                       rows: rows
-                   });
-    });
-     **/
+
 });
 
 router.post("/", function(req, res){
 
-    console.log("check");
+    logger.info("check");
     var token = req.session.user;
     if(token === undefined) {
-        console.log("not login");
+        logger.info("not login");
         res.json({"status":"1", msg: "not login"});
     }
     var _id = req.body.id;
     var _keyword = req.body.keyword;
     var _trash = req.body.trash;
 
-    console.log([
-        "id: ", _id,
-        "keyword: ", _keyword,
-        "trash: ", _trash
+    logger.info([
+        "_id",
+        _id,
+        "keyword: ",
+        _keyword,
+        "_trash: ",
+        _trash
     ]);
-    console.log(["_id: ", _id, "keyword: ", _keyword, "_trash: ", _trash]);
+
     if (_trash == 0) {
         var _username = req.body.username;
         var _labels = req.body.labels;
         // upate the valid of target samples
-        console.log([_username, _labels, "checkConflict"]);
+        logger.info([
+            _username,
+            _labels,
+            "checkConflict"
+        ]);
+        logger.info([_username, _labels, "checkConflict"]);
         samples.checkConflict(_keyword, _id, _username, _labels, function(status, msg){
             res.json({"status": status, "msg": msg});
         });
 
     } else {
-        console.log(_id);
+        logger.info(_id);
         var thread = new Thread(_id, _keyword);
         thread.trash(function (status, msg) {
             res.json({'status': status, 'msg': msg});
--- Labeler/routes/index.js
+++ Labeler/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var session = require("express-session");
+var logger = require("../models/logger");
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
     // check if has login
     if(req.session.user != undefined) {
+
+        logger.info(req.session.user);
         console.log(req.session.user);
         if (req.session.isSuper != undefined) {
             res.redirect("/overview");
